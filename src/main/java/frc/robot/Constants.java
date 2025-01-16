e165af3f74a6a36bf30120f381570a1834b550b6
perry <shubdeshpa@gmail.com>
2025-01-16 17:38:33 -0600

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public class DriveConstants {
+        // CAN ID
+        private static final int leftFrontMotorCANID = 0; // fix later
+        private static final int leftBackMotorCANID = 1;
+        private static final int rightFrontMotorCANID = 2;
+        private static final int rightBackMotorCANID = 3;
+        
+        // PID Constants
+        private static final double kP = 0;
+        private static final double kI = 0;
+        private static final double kD = 0;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/TankDrive.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+
+import frc.robot.Constants.DriveConstants;
+
+public class TankDrive extends SubsystemBase {
+
+    
+
+    public TankDrive() {
+
+    }
+}
